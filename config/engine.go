a3b3ae3e1d714f33b8ba958ecb383e1ba4c294a6
Vasyl Tretiakov <vasyl.tretiakov@gmail.com>
2025-10-28 22:35:56 +0200

    remove obsolete engine.maxFrames config parameter

--- config/engine.go
+++ config/engine.go
@@ -90,7 +90,6 @@ type EngineConfig struct {
 	ProvingKeyId         string `yaml:"provingKeyId"`
 	Filter               string `yaml:"filter"`
 	GenesisSeed          string `yaml:"genesisSeed"`
-	MaxFrames            int64  `yaml:"maxFrames"`
 	PendingCommitWorkers int64  `yaml:"pendingCommitWorkers"`
 	MinimumPeersRequired int    `yaml:"minimumPeersRequired"`
 	StatsMultiaddr       string `yaml:"statsMultiaddr"`
